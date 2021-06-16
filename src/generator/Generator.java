ca6c876f8c83143fd89583224d7a12f10d9ae436
Youmiel <Youmiel@qq.com>
2021-06-16 13:14:52 +0800

    Add Main

--- src/generator/Generator.java
+++ src/generator/Generator.java
@@ -64,23 +64,5 @@ public class Generator {
 		SourceIO.writeImage(texture, OUTPUT_PATH + "ct/" + this.sourceName + "_ctm.png");
 		SourceIO.writeJSON(this.json, OUTPUT_PATH + "json/" + this.sourceName + ".png.mcmeta");
 	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		File[] fileList = new File(INPUT_PATH).listFiles(new FilenameFilter() {
-			@Override
-			public boolean accept(File dir,String name) {
-				return name.toLowerCase().endsWith(".png");
-			}
-		});
-		Generator gen = new Generator();
-		for(int i = 0; i<fileList.length; i++) {
-			String fileName = fileList[i].getName();
-			String name = fileName.substring(0,fileName.indexOf('.'));
-			gen.setSource(fileList[i].getPath(),name);
-			gen.generateTexture();
-			System.out.println("[" + (i+1)+ "] " + name + " completed.");
-		}
-	}
 
 }
